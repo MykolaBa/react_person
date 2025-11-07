10d48175db4b59d08f612d54bfa02b22fc5851a3
Микола. <basovmikola04@gmail.com>
2025-11-07 16:13:35 +0200

    fixed

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,16 +1,23 @@
-export const Person = ({ person }) => (
-  <section className="Person">
-    <h2 className="Person__name">{`My name is ${person.name}`}</h2>
+export const Person = ({ person }) => {
+  const { name, age, sex, isMarried, partnerName } = person;
 
-    {person.age && <p className="Person__age">{`I am ${person.age}`}</p>}
+  let partnerStatus;
 
-    <p className="Person__partner">
-      {/* eslint-disable-next-line no-nested-ternary */}
-      {!person.isMarried
-        ? 'I am not married'
-        : person.sex === 'm'
-          ? `${person.partnerName} is my wife`
-          : `${person.partnerName} is my husband`}
-    </p>
-  </section>
-);
+  if (!isMarried) {
+    partnerStatus = 'I am not married';
+  } else if (sex === 'm') {
+    partnerStatus = `My wife ${partnerName}`;
+  } else {
+    partnerStatus = `My husband ${partnerName}`;
+  }
+
+  return (
+    <section className="Person">
+      <h2 className="Person__name">{`My name is ${name}`}</h2>
+
+      {age && <p className="Person__age">{`I am ${age}`}</p>}
+
+      <p className="Person__partner">{partnerStatus}</p>
+    </section>
+  );
+};
